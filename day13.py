d2a41a3e4a12fd8d5d4dcc93e443ca9f1bd64a58
snehalyadav04 <snehaly40@gmail.com>
2025-08-27 18:38:34 +0530

    Create day13.py
    Day 13 Challenge

---
+++ day13.py
@@ -0,0 +1,42 @@
+def longest_palindrome(s: str) -> str:
+    if len(s) == 0:
+        return ""
+
+    start = 0
+    end = 0
+
+    def expand_around_center(left: int, right: int) -> tuple:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+
+    for i in range(len(s)):
+        left1, right1 = expand_around_center(i, i)
+        left2, right2 = expand_around_center(i, i + 1)
+
+        if right1 - left1 > end - start:
+            start, end = left1, right1
+        if right2 - left2 > end - start:
+            start, end = left2, right2
+
+    return s[start:end+1]
+
+print("Input: \"babad\"")
+print("Output:", longest_palindrome("babad"))
+print()
+
+print("Input: \"cbbd\"")
+print("Output:", longest_palindrome("cbbd"))
+print()
+
+print("Input: \"a\"")
+print("Output:", longest_palindrome("a"))
+print()
+
+print("Input: \"aaaa\"")
+print("Output:", longest_palindrome("aaaa"))
+print()
+
+print("Input: \"abc\"")
+print("Output:", longest_palindrome("abc"))
