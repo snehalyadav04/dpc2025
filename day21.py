acfb6b10e316d583acf75c18994601df5642e4ff
snehalyadav04 <snehaly40@gmail.com>
2025-09-04 20:32:26 +0530

    Create day21.py
    Day 21 Challenge

---
+++ day21.py
@@ -0,0 +1,22 @@
+def sort_list(arr):
+    return sorted(arr)
+
+test1 = [3, 2, 1]
+print("Input:", test1)
+print("Output:", sort_list(test1))
+
+test2 = [5]
+print("Input:", test2)
+print("Output:", sort_list(test2))
+
+test3 = [-5, -10, -15]
+print("Input:", test3)
+print("Output:", sort_list(test3))
+
+test4 = []
+print("Input:", test4)
+print("Output:", sort_list(test4))
+
+test5 = [3, 3, 3]
+print("Input:", test5)
+print("Output:", sort_list(test5))
